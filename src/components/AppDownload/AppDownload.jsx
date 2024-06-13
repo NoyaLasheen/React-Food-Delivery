805ed5e206e27892087f5756479fcf85da1327db
Noya Lasheen <noyalasheen@gmail.com>
2024-06-13 13:49:41 +0530

    App Download feature and styling

---
+++ src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,19 @@
+// import React from "react";
+import { assets } from "../../assets/assets";
+import "./AppDownload.css";
+
+const AppDownload = () => {
+  return (
+    <div className="app-download" id="app-download">
+      <p>For better experience download</p>
+      <br />
+      Tomato App
+      <div className="app-download-platforms">
+        <img src={assets.play_store} alt="" />
+        <img src={assets.app_store} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default AppDownload;
