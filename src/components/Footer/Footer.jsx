896de5a87e2003975199973a523f2f08cb07f05d
Noya Lasheen <noyalasheen@gmail.com>
2024-06-13 13:15:06 +0530

    Added footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,47 @@
+// import React from "react";
+import { assets } from "../../assets/assets";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum magni
+            corrupti quos, deserunt incidunt ea aliquid ex recusandae amet quis
+            eligendi iste porro enim maxime fuga. Soluta dolores atque odio?
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>Get in touch</h2>
+          <ul>
+            <li>903456778</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 &copyright; Tomato.com - All Rights Reserved
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
